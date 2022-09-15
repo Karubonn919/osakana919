7e369b4e37b07a6c7b0c752936ccf0bb72320b53
Karubonn919 <89884995+Karubonn919@users.noreply.github.com>
2022-09-15 14:23:50 +0900

    Add files via upload

---
+++ assets/js/anime.js
@@ -0,0 +1,91 @@
+$(function(){
+    $(window).on('scroll',function (){
+      let triggerClass = $('.gimi');
+          animateClass = ('is-show');
+   
+      $(triggerClass).each(function(){
+        let scroll       = $(window).scrollTop(),
+            triggerTop   = $('.sc').offset().top,
+            windowHeight = $(window).height();
+        if (scroll > triggerTop - windowHeight/1.8){
+          $(this).addClass("opc1");
+        }
+      });
+    });
+  });
+  $(function(){
+    $(window).on('scroll',function (){
+      let triggerClass = $('.gimi0');
+          animateClass = ('is-show');
+   
+      $(triggerClass).each(function(){
+        let scroll       = $(window).scrollTop(),
+            triggerTop   = $('.sc').offset().top,
+            windowHeight = $(window).height();
+        if (scroll > triggerTop - windowHeight/1.8){
+          $(this).addClass("opc");
+        }
+      });
+    });
+  });
+  $(function(){
+    $(window).on('scroll',function (){
+      let triggerClass = $('.gimi1');
+          animateClass = ('is-show');
+   
+      $(triggerClass).each(function(){
+        let scroll       = $(window).scrollTop(),
+            triggerTop   = $('.sc').offset().top,
+            windowHeight = $(window).height();
+        if (scroll > triggerTop - windowHeight/1.8){
+          $(this).addClass("opc0");
+        }
+      });
+    });
+  });
+  $(function(){
+    $(window).on('scroll',function (){
+      let triggerClass = $('.gimi2');
+          animateClass = ('is-show');
+   
+      $(triggerClass).each(function(){
+        let scroll       = $(window).scrollTop(),
+            triggerTop   = $('.sc').offset().top,
+            windowHeight = $(window).height();
+        if (scroll > triggerTop - windowHeight/1.8){
+          $(this).addClass("opc2");
+        }
+      });
+    });
+  });
+  $(function(){
+    $(window).on('scroll',function (){
+      let triggerClass = $('.gimibox');
+          animateClass = ('is-show');
+   
+      $(triggerClass).each(function(){
+        let scroll       = $(window).scrollTop(),
+            triggerTop   = $('.sc').offset().top,
+            windowHeight = $(window).height();
+        if (scroll > triggerTop - windowHeight/1.8){
+          $(this).addClass("opc");
+        }
+      });
+    });
+  });
+  $(function(){
+    $(window).on('scroll',function (){
+      let triggerClass = $('.gimitex');
+          animateClass = ('is-show');
+   
+      $(triggerClass).each(function(){
+        let scroll       = $(window).scrollTop(),
+            triggerTop   = $('.sc').offset().top,
+            windowHeight = $(window).height();
+        if (scroll > triggerTop - windowHeight/1.8){
+          $(this).addClass("opc");
+        }
+      });
+    });
+  });
+  
